4b83f461c80793b9d221c0f18cad8dedee362c07
Joe Benz <89860209+joe-dcs@users.noreply.github.com>
2021-12-20 10:39:47 -0500

    Add files via upload

---
+++ shinyInfo.R
@@ -0,0 +1,5 @@
+rsconnect::setAccountInfo(name='joe-dcs', token='4F9531A2CA9D22F5110330B3F25914C3', secret='RKxuxkVTYcZmOmtqFPOyrQUNyMNW+CewI1jfqSvt')
+
+library(rsconnect)
+rsconnect::deployApp()
+Y
